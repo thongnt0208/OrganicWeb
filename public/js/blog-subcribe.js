0a826b1c46a0f5e6f2df97e45059b253ffbf7011
dothehieu.92.20172018@gmail.com <dothehieu.92.20172018@gmail.com>
2022-08-23 15:18:06 +0700

    Blog + subcribe - lack of @media

---
+++ public/js/blog-subcribe.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){new Splide(".splide.splide__blog",{type:"loop",perPage:3,perMove:1,breakpoints:{1180:{perPage:2},820:{perPage:1}}}).mount()});
